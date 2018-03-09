7a57c5e1bdc16258be8540e30eec65c89191a5af
kvibber <kv@tollfreeforwarding.com>
2018-03-09 11:08:32 -0800

    Add Javadoc comments so that we can submit the artifacts to the central repository

--- src/com/communicate/LongId.java
+++ src/com/communicate/LongId.java
@@ -1,25 +1,4 @@
-package com.communicate;
-
-import java.util.Date;
-
 /*
- * Generates a unique ID based on milliseconds and an optional server Id.
- * Builds the following as a hex string and converts to a long:
- *    (11 hex digits timestamp) (2 hex digits counter) (3 hex digits server ID)
- * Server IDs can range from 0 - 4095
- * A counter resolves collisions where two or more IDs are created within the same millisecond.
- * We can extract the timestamp or server ID from any ID generated using this scheme.
- *
- * Usage:
- *
- *   LongId li = new LongId(1) - creates an instance to generate IDs with the embedded server ID 1.
- *   li.getNewId() - generates IDs with the embedded server Id on the instance.
- *
- * Once you have a generated id, you can retrieve information from it, with or without an instance.
- *
- *   LongId.getDate(id) - extract the timestamp from the ID.
- *   LongId.getServerId(id) - extract the server ID from the ID.
- *
  * MIT License
  *
  * Copyright (c) 2018 Communicate.com
@@ -43,6 +22,43 @@ import java.util.Date;
  * SOFTWARE.
  */
 
+package com.communicate;
+
+import java.util.Date;
+
+/**
+ * Generates a unique numeric ID based on current milliseconds and an optional server/instance ID.
+ * Suitable for use as a database primary key in a distributed app, as new IDs are guaranteed
+ * to be unique and will always be added at or near the end of the table.
+ * <p>
+ * Builds the following as a hex string and converts to a long:
+ *    <code>(11 hex digits timestamp) (2 hex digits counter) (3 hex digits server ID)</code>
+ * Server IDs can range from 0 - 4095.
+ * A counter resolves collisions where two or more IDs are created within the same millisecond.
+ * We can extract the timestamp or server ID from any ID generated using this scheme.
+ * </p>
+ *
+ * <p>
+ *     Usage:
+ * </p>
+ *
+ * <ul>
+ *   <li>LongId lid = new LongId() - creates an instance to generate IDs with the embedded server ID 0.
+ *   <li>LongId lid = new LongId(123) - creates an instance to generate IDs with the embedded server ID 123.
+ *   <li>lid.getNewId() - generates IDs with the embedded server ID on the instance.
+ * </ul>
+ *
+ * <p>
+ *     Once you have a generated id, you can retrieve information from it, with or without an instance.
+ * <ul>
+ *   <li>LongId.getDate(id) - extract the timestamp from the ID.
+ *   <li>LongId.getServerId(id) - extract the server ID from the ID.
+ * </ul>
+ *
+ * @author      Communicate.com kv@communicate.com
+ * @version     1.0
+ * @since       1.0
+ */
 public class LongId {
 
     // How many hex digits we use for each component of the ID
@@ -64,11 +80,18 @@ public class LongId {
     // Optional server ID will be 0. Can be set by creating a new LongId(serverId);
     private final long serverId;
 
-    // Create a new instance and set the server Id to 0.
+    /**
+     * Create a new instance and set the server Id to 0.
+     */
     public LongId() {
         serverId = 0;
     }
-    // Create a new instance and set the server Id.
+
+    /**
+     * Create a new instance for a specific server/app instance. Ensures each instance generates a unique set of IDs.
+     *
+     * @param  setServerId Numeric ID indicating which server is generating this ID.
+     */
     public LongId(long setServerId) {
         if (setServerId > SERVER_MAX || setServerId < 0) {
             throw new IllegalArgumentException("Server Id must be in the range 0-" + SERVER_MAX);
@@ -76,13 +99,24 @@ public class LongId {
         serverId = setServerId;
     }
 
-    // Generate a new ID with the given instance's server ID
+    /**
+     * Generate a new ID with the given instance's server ID
+     *
+     * @return A unique ID suitable for use as a database key
+     */
     public long getNewId() {
         return getNewIdStatic(serverId);
     }
 
-    // Generate a new ID. Synchronized so that each thread will wait for the previous one to finish, allowing us
-    // to maintain state and guarantee a unique ID when two threads hit within the same millisecond.
+    /**
+     * Generate a new ID. Synchronized so that each thread will wait for the previous one to finish, allowing us
+     * to maintain state and guarantee a unique ID when two threads hit within the same millisecond.
+     *
+     * If we hit the counter limit within a millisecond, we sleep for a millisecond and start over.
+     *
+     * @param serverId The server ID of the instance calling this method.
+     * @return A unique ID suitable for use as a database key
+     */
     private static synchronized long getNewIdStatic(long serverId) {
         // store the current millis since epoch
         long millisCurrent = System.currentTimeMillis();
@@ -125,27 +159,43 @@ public class LongId {
         return Long.decode("0x" + millisAsHex + counterAsHex + serverAsHex);
     }
 
-    // Get the date that a LongId was generated.
-    // Drop the last 6 hex digits and the rest will be the timestamp
-    public static Date getDate(long microId) {
-        String hexInput = Long.toHexString(microId);
+    /**
+     * Get the date/time that a LongId was generated.
+     *
+     * @param  longId The numeric form of a LongId generated with this class. Does not have to be generated by this instance.
+     * @return Date object representing the time that the LongId was generated.
+     */
+    public static Date getDate(long longId) {
+        // Convert to hex, then drop the last 6 hex digits. The rest will be the timestamp
+        String hexInput = Long.toHexString(longId);
         if (hexInput.length() < COUNTER_HEX_DIGITS+SERVER_HEX_DIGITS + 1)
             throw new IllegalArgumentException("Input is too short to be a LongId");
         return new Date(Long.decode("0x" + hexInput.substring(0, hexInput.length()-(COUNTER_HEX_DIGITS+SERVER_HEX_DIGITS) )));
     }
 
-    // Get the server on which a LongId was generated.
-    // Convert number to hex. Take the last few hex digits. Convert them back to a number. That's the server Id.
-    public static long getServerId(long microId) {
-        String hexInput = Long.toHexString(microId);
+    /**
+     * Get the server/instance ID on which a LongId was generated.
+     *
+     * @param  longId The numeric form of a LongId generated with this class. Does not have to be generated by this instance.
+     * @return Numeric ID of the server/instance that generated the LongId
+     */
+    public static long getServerId(long longId) {
+        // Convert number to hex. Take the last few hex digits. Convert them back to a number. That's the server Id.
+        String hexInput = Long.toHexString(longId);
         if (hexInput.length() < COUNTER_HEX_DIGITS+SERVER_HEX_DIGITS + 1)
             throw new IllegalArgumentException("Input is too short to be a LongId");
         return Long.decode("0x" + hexInput.substring(hexInput.length()-SERVER_HEX_DIGITS) );
     }
 
-    // Get the counter from a LongId. Not significant except for debugging.
-    public static long getCounter(long microId) {
-        String hexInput = Long.toHexString(microId);
+    /**
+     * Get the same-millisecond counter from a LongId. Not userful except for debugging.
+     *
+     * @param  longId The numeric form of a LongId generated with this class. Does not have to be generated by this instance.
+     * @return Numeric counter
+     */
+    public static long getCounter(long longId) {
+        // Convert to hex, remove the server digits, then look at the last few remaining for the hex digits.
+        String hexInput = Long.toHexString(longId);
         if (hexInput.length() < COUNTER_HEX_DIGITS+SERVER_HEX_DIGITS + 1)
             throw new IllegalArgumentException("Input is too short to be a LongId");
         return Long.decode("0x" + hexInput.substring(hexInput.length()-(COUNTER_HEX_DIGITS+SERVER_HEX_DIGITS),hexInput.length()-SERVER_HEX_DIGITS) );
